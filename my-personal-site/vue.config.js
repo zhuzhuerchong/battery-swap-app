380c110c5ee8818bb487fab709d2d6780c7aa0b0
zhuzhuerchong <woshiyigeshejishi@gmail.com>
2024-08-14 14:28:20 +0000

    Update vue.config.js

--- my-personal-site/vue.config.js
+++ my-personal-site/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    https: false, // Ensure HTTPS is turned off
+    port: 8080,   // Ensure the correct port is specified
+    client: {
+      webSocketURL: {
+        port: 8080,
+        hostname: 'localhost',
+        pathname: '/ws',
+        protocol: 'ws',
+      },
+    },
+    allowedHosts: 'all', // Allow connections from any origin
+  }
+}
